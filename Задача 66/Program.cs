e4a6ea7cd1ff5b66dd50d6148bb75e8c969a781a
Vadim <nikegufo666@gmail.com>
2023-07-16 10:14:05 +0500

    Добавил задачи

---
+++ Задача 66/Program.cs
@@ -0,0 +1,28 @@
+﻿int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void SumPrint(int M, int N)
+{
+    Console.Write(SumMN(M - 1, N));
+}
+
+int SumMN(int M, int N)
+{
+    int res = M;
+    if (M == N)
+        return 0;
+    else
+    {
+        M++;
+        res = M + SumMN(M, N);
+        return res;
+    }
+}
+
+
+int M = ReadInt("Начальное");
+int N = ReadInt("Конечное");
+SumPrint(M, N);
---
+++ Задача 68/Program.cs
@@ -0,0 +1,30 @@
+﻿int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void SumPrint(int M, int N)
+{
+    Console.Write(Akkerman(M, N));
+}
+
+int Akkerman(int M, int N)
+{
+    if (M == 0)
+    {
+        return N + 1;
+    }
+    else if (N == 0 && M > 0)
+    {
+        return Akkerman(M - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(M - 1, Akkerman(M, N - 1)));
+    }
+}
+
+int M = ReadInt("Начальное");
+int N = ReadInt("Конечное");
+SumPrint(M, N);
